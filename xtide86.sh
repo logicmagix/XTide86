950d1171485e68446137099d925d92302e1ea79a
logicmagix <logicmagix@protonmail.com>
2025-05-29 14:46:33 -0400

    Fixed typos in xtide86.sh

--- xtide86.sh
+++ xtide86.sh
@@ -1,21 +1,21 @@
 
-" xtide86 - a terminal IDE powered by tmux and nvim
-" Copyright (C) 2025 Pavle Dzakula
-"
-" This program is free software: you can redistribute it and/or modify
-" it under the terms of the GNU General Public License as published by
-" the Free Software Foundation, either version 3 of the License, or
-" (at your option) any later version.
-"
-" This program is distributed in the hope that it will be useful,
-" but WITHOUT ANY WARRANTY; without even the implied warranty of
-"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the# GNU General Public License for more details.
-"
-" You should have received a copy of the GNU General Public License
-" along with this program. If not, see <https://www.gnu.org/licenses/>.
-" Credits
+# xtide86 - a terminal IDE powered by tmux and nvim
+# Copyright (C) 2025 Pavle Dzakula
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# Credits
 
-"This project includes `termic.sh` from [Yusuf Kagan Hanoglu/Max Schillinger/TermiC], licensed under the [GPL3] License.
+#This project includes `termic.sh` from [Yusuf Kagan Hanoglu/Max Schillinger/TermiC], licensed under the [GPL3] License.
 
 #!/usr/bin/env bash
 set -e
